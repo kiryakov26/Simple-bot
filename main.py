4cb6971c5d8676b712d0a3bdc33afbc067cdf7db
kiryakov26 <99785060+kiryakov26@users.noreply.github.com>
2022-02-16 09:49:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ def start(message):
 @bot.message_handler(commands=['help'])
 def start_message(message):
     keyboard = types.ReplyKeyboardMarkup()
-    keyboard.row("/buy", "Хочу", "История","Обзор","/start")
+    keyboard.row("/buy", "Хочу","/check" ,"История","Обзор","/start")
     bot.send_message(message.chat.id, 'Если хочешь заказать конструктор, жми buy \n'
+                                      'Хотите узнать стоимость заказа в рублях, жми check \n'
                                       'Хочешь узнать о новинках Lego, пиши Хочу \n'
                                       'Хочешь узнать про историю компании Lego, пиши История \n'
                                       'Хочешь прочитать обзоры на наборы, пиши Обзор \n'
@@ -26,6 +27,12 @@ def start(message):
     bot.send_message ( message.chat.id, 'Тогда тебе сюда – https://lego.com')
     bot.send_message ( message.chat.id, 'Вернуться к возможностям, жми help', reply_markup=keyboard)
 
+@bot.message_handler(commands=['check'])
+def converter_mode_0(message):
+    bot.send_message(message.chat.id, 'USD->RUB , Введите сумму заказа')
+    global i
+    i = 0
+
 @bot.message_handler(content_types=['text'])
 def answer(message):
     if message.text.lower() == "хочу":
@@ -34,4 +41,10 @@ def answer(message):
         bot.send_message(message.chat.id, 'Тогда тебе сюда – https://ru.wikipedia.org/wiki/LEGO')
     elif message.text.lower() == "обзор":
         bot.send_message(message.chat.id, 'Тогда тебе сюда – http://bricker.ru')
+    elif i == 0:
+        keyboard = types.ReplyKeyboardMarkup()
+        keyboard.row("/help")
+        bot.send_message(message.chat.id, round(float(float(message.text) * 75.24), 2))
+        bot.send_message(message.chat.id, 'Чтобы продолжить, пиши следующуюю сумму: \n'
+                                          'Вернуться к возможностям, жми help', reply_markup=keyboard)
 bot.polling()
